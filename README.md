3f2d8648cf3981fc3f2b9ee805a6b8633251d181
YouTubeLuisangelponce <poncealvarezluisangel01@gmail.com>
2023-06-09 21:45:47 -0600

    añado readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Portafolio
+
+[SitioWeb](https://portafolioyoutube.netlify.app/)
+
+## Descrición
+
+Este es un sitio web desarrollado con React en un tutorial es un portafolio de desarrollador web
+en el proyecto utilizamos
+
+1. Vite
+2. React
+3. Git
+4. Github
+5. Netlify
+6. Bootstrap
+7. Css
+8. Html
+9. Javascript
+
+## Miniatura
+
+![Miniatura](/public/Miniatura.png)
+
+### Miniatura grande
+
+![sitio](/public/grande.png)
--- src/Components/Inicio/index.jsx
+++ src/Components/Inicio/index.jsx
@@ -6,10 +6,11 @@ function Inicio() {
     <div className="container-inicio d-flex aling-items-center justify-content-center mt-5 p-5">
       <div className="text">
         <h4>👋Hola, soy</h4>
-        <h1>Braian</h1>
+        <h1>Luis Angel Ponce Alvarez</h1>
         <h5>
           <>Desarrollador Web</>
         </h5>
+        <p>Este es un portafolio de un tutorial</p>
       </div>
       <div className="img">
         <img src={img} alt="img" />
